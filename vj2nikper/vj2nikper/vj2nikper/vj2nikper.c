11177123108e17117251b621d6f89f49791882c1
nikolica2 <anikol01@fesb.hr>
2023-10-25 17:13:10 +0200

    update

--- vj2nikper/vj2nikper/vj2nikper/vj2nikper.c
+++ vj2nikper/vj2nikper/vj2nikper/vj2nikper.c
@@ -1,3 +1,10 @@
+﻿/*2. Definirati strukturu osoba(ime, prezime, godina rođenja) i napisati program koji :
+A.dinamički dodaje novi element na početak liste,
+B.ispisuje listu,
+C.dinamički dodaje novi element na kraj liste,
+D.pronalazi element u listi(po prezimenu),
+E.briše određeni element iz liste,
+U zadatku se ne smiju koristiti globalne varijable.*/
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
